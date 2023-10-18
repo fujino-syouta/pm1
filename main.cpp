bdb65f4616a353a79b8099e95fb23d64e4538b1f
翔太 藤野 <k023g1052@g.neec.ac.jp>
2023-10-18 10:48:23 +0900

    35行目のコメントアウトを削除

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		/// 
+
 		Novice::DrawEllipse(50, 50, 20, 20, 0.0f, WHITE, kFillModeWireFrame);
 		Novice::DrawBox(50, 50, 30, 30, 0.0f, RED, kFillModeSolid);
 		Novice::DrawEllipse(PlayerPosx, 150, 20, 20, 0.0f, GREEN, kFillModeSolid);
